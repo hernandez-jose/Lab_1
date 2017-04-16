e44a71d66ada469587c01703c86670d3a6a1bea4
josedhnaranjo <josedhnaranjo@gmail.com>
2017-04-16 16:50:39 -0700

    Wallet class no longer needs to be a pointer

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -4,34 +4,32 @@ Lab_1.cpp : Defines the entry point for the console application.
 #include "stdafx.h"
 #include<iostream>
 #include<string>
-#include "Currency.h"
 #include "Wallet.h"
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Wallet* bag;
-	bag = new Wallet;
+	Wallet bag;
+	
 
-	bag->setWorkingCode("USD");
-	bag->setWorkingWholeName("US Dollar");
-	bag->setWorkingFractionName("Cents");
-	bag->setWorkingOperation("addition");
+	bag.setWorkingCode("USD");
+	bag.setWorkingWholeName("US Dollar");
+	bag.setWorkingFractionName("Cents");
+	bag.setWorkingOperation("addition");
 
 	cout << "Enter Amount" << endl;
 
 	cin >> bag;
 
-	bag->setWorkingCode("MXN");
-	bag->setWorkingWholeName("Peso");
-	bag->setWorkingFractionName("Centavos");
-	bag->setWorkingOperation("addition");
+	bag.setWorkingCode("MXN");
+	bag.setWorkingWholeName("Peso");
+	bag.setWorkingFractionName("Centavos");
+	bag.setWorkingOperation("addition");
 
 	cin >> bag;
 
 	cout << bag;
-	delete bag;
 	
 	system("pause");
 
